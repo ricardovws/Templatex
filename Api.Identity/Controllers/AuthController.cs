f844e84b1cac5bee5a8075ce69c1c92b41c103d3
Ricardo <ricardovws@gmail.com>
2021-06-10 18:44:39 -0300

    iniciando identity api

---
+++ Api.Identity/Controllers/AuthController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using NetDevPack.Identity.Jwt;
+using NetDevPack.Identity.Jwt.Model;
+using NetDevPack.Identity.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Identity.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppJwtSettings _appJwtSettings;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, IOptions<AppJwtSettings> appJwtSettings)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _appJwtSettings = appJwtSettings.Value;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterUser registerUser)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok(GetUserResponse(user.Email));
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginUser loginUser)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+            if (result.Succeeded)
+            {
+                return Ok(GetFullJwt(loginUser.Email));
+            }
+
+            if (result.IsLockedOut)
+            {
+                return BadRequest("This user is blocked");
+            }
+
+            return BadRequest("Incorrect user or password");
+        }
+
+        private string GetFullJwt(string email)
+        {
+            return new JwtBuilder()
+                .WithUserManager(_userManager)
+                .WithJwtSettings(_appJwtSettings)
+                .WithEmail(email)
+                .WithJwtClaims()
+                .WithUserClaims()
+                .WithUserRoles()
+                .BuildToken();
+        }
+
+        private UserResponse<string> GetUserResponse(string email)
+        {
+            return new JwtBuilder()
+                .WithUserManager(_userManager)
+                .WithJwtSettings(_appJwtSettings)
+                .WithEmail(email)
+                .WithJwtClaims()
+                .WithUserClaims()
+                .WithUserRoles()
+                .BuildUserResponse();
+        }
+    }
+}
